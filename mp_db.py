4bf445fe955a5097fd67ff2b0c44d6d5b48d8666
JonahWeinbaum <81923472+JonahWeinbaum@users.noreply.github.com>
2023-11-03 14:42:10 -0400

    Added parallelization!

--- mp_db.py
+++ mp_db.py
@@ -4,6 +4,9 @@ from urllib.parse import quote_plus
 from scrape import get_route, get_area, get_id, get_directory
 from logger import lprint, lpprint
 from resume import find_root_parent_id
+from multiprocessing.pool import ThreadPool as Pool
+from multiprocessing import cpu_count
+from joblib import Parallel, delayed
 
 username = quote_plus('EvilMonkey')
 password = quote_plus('&a@JREztYS5@EyPL')
@@ -21,7 +24,7 @@ except Exception as e:
 
 db = client["mountain_project"]
 
-def populate_routes_in(areas, routes, area_id):
+def populate_routes_in(areas, routes, area_id, worker_id = -1):
     area = get_area(area_id)
     area_processed = process_area(area)
 
@@ -29,10 +32,16 @@ def populate_routes_in(areas, routes, area_id):
     if area_exists is None:
         # Object doesn't exist, so add it to the collection
         result = areas.insert_one(area_processed)
-        lprint("New area added with id: " + str(result.inserted_id))
+        if (worker_id == -1):
+            lprint("New area added with id: " + str(result.inserted_id))
+        else: 
+            lprint(f"[Worker {worker_id}] New area added with id: " + str(result.inserted_id))
     else:
         # Object with the same name already exists; you can update it or take other action
-        lprint(f"Area {area_id} already exists.")
+        if (worker_id == -1):
+            lprint(f"Area {area_id} already exists.")
+        else: 
+            lprint(f"[Worker {worker_id}] Area {area_id} already exists.")
 
     for child in area['children']:
 
@@ -48,13 +57,19 @@ def populate_routes_in(areas, routes, area_id):
             if route_exists is None:
                 # Object doesn't exist, so add it to the collection
                 result = routes.insert_one(route)
-                lprint("New route added with id: " + str(result.inserted_id))
+                if (worker_id == -1):
+                    lprint("New route added with id: " + str(result.inserted_id))
+                else: 
+                    lprint(f"[Worker {worker_id}] New route added with id: " + str(result.inserted_id))
             else:
                 # Object with the same name already exists; you can update it or take other action
-                lprint(f"Route {child_id} already exists.")
+                if (worker_id == -1):
+                    lprint(f"Route {child_id} already exists.")
+                else: 
+                    lprint(f"[Worker {worker_id}] Route {child_id} already exists.")
 
         else: 
-            populate_routes_in(areas, routes, child_id)
+            populate_routes_in(areas, routes, child_id, worker_id)
 
 def populate_routes(db, start_id = 105905173):
     started = False
@@ -69,6 +84,38 @@ def populate_routes(db, start_id = 105905173):
         if (started):
             populate_routes_in(areas, routes, area_id)
 
+def directory_index(directory, area_id):
+    index = None
+    for i, state in enumerate(directory):
+        if get_id(directory[state]) == area_id:
+            index = i
+    return index
+
+def worker(state, db, start_id, areas, routes, directory, worker_id):
+    area_id = get_id(directory[state])
+    if directory_index(directory, area_id) >= directory_index(directory, find_root_parent_id(db, start_id)):
+        populate_routes_in(areas, routes, area_id, worker_id)
+
+def parallel_populate_routes(db, start_id=105905173):
+    areas = db['areas']
+    routes = db['routes']
+    directory = get_directory()
+    del directory['International']
+
+    # Create a pool of worker processes
+    pool = Pool(processes=cpu_count())  # Use the number of CPU cores available
+
+    worker_ids = range(pool._processes)  # Generate worker identifiers
+
+    # Use the pool of processes to parallelize the work
+    # lprint([(worker_id, state) for worker_id, state in zip(, directory)])
+    pool.starmap(worker, [(state, db, start_id, areas, routes, directory, worker_id) for worker_id, state in zip(list(worker_ids) * (len(directory) // len(worker_ids)), directory)])
+    pool.close()
+    pool.join()
+
+
+    return
+
 def process_area(area):
     area_copy = area.copy()
     del area_copy['children']
@@ -81,4 +128,4 @@ def process_route(route):
     return route_copy
 
 last_checked_area = 112559685
-populate_routes(db, last_checked_area)
+parallel_populate_routes(db, last_checked_area)
